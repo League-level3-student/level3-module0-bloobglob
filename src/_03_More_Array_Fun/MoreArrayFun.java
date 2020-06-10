964dfe37695fae409705e31f1e634949d927169e
bloobglob <50851263+bloobglob@users.noreply.github.com>
2020-06-10 16:07:39 -0700

    More Array Fun

--- src/_03_More_Array_Fun/MoreArrayFun.java
+++ src/_03_More_Array_Fun/MoreArrayFun.java
@@ -1,5 +1,6 @@
 package _03_More_Array_Fun;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 public class MoreArrayFun {
@@ -8,6 +9,8 @@ public class MoreArrayFun {
 		String[] strings = {"hello", "my", "name", "is", "Lucas"};
 		new MoreArrayFun().printStrings(strings);
 		new MoreArrayFun().printBackwardsStrings(strings);
+		new MoreArrayFun().printEveryOtherString(strings);
+		new MoreArrayFun().printRandomString(strings);
 	}
 	
 	
@@ -15,24 +18,46 @@ public class MoreArrayFun {
 	
 	void printStrings(String[] strings) {
 		for(int i = 0; i<strings.length; i++) {
-			System.out.println();	
+			System.out.println(strings[i]);	
 		}
+		System.out.println();
 	}
 	
 	//3. Write a method that takes an array of Strings and prints all the Strings in the array
 	//   in reverse order.
 	
 	void printBackwardsStrings(String[] strings) {
-		for(int i = strings.length-1; i>0; i--) {
-			System.out.println();	
+		for(int i = strings.length-1; i>=0; i--) {
+			System.out.println(strings[i]);	
 		}
+		System.out.println();
 	}
 	
 	//4. Write a method that takes an array of Strings and prints every other String in the array.
-	
+
+	void printEveryOtherString(String[] strings) {
+		for(int i = 0; i<strings.length; i++) {
+			if(i%2==0) {
+				System.out.println(strings[i]);	
+			}
+		}
+		System.out.println();
+	}
 	
 	//5. Write a method that takes an array of Strings and prints all the Strings in the array
 	//   in a completely random order. Almost every run of the program should result in a different order.
 	
+	void printRandomString(String[] strings) {
+		ArrayList<String> stringsPrinted = new ArrayList<String>();
+		int random;
+		while(stringsPrinted.size() < 5) {
+			random = new Random().nextInt(5);
+			if(!stringsPrinted.contains(strings[random])) {
+				System.out.println(strings[random]);
+				stringsPrinted.add(strings[random]);
+			}
+		}
+		System.out.println();
+	}
 	
 }
