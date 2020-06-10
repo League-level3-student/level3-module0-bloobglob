d45b9f9cbf1602dd99abcbe302d3e3e4e66e9544
bloobglob <50851263+bloobglob@users.noreply.github.com>
2020-06-10 15:12:57 -0700

    1D array Methods, Firework. More Array Fun (IP)

--- src/_01_Simple_Array_Algorithms/_00_1D_Array_Methods.java
+++ src/_01_Simple_Array_Algorithms/_00_1D_Array_Methods.java
@@ -4,15 +4,22 @@ public class _00_1D_Array_Methods {
 	//1. Complete the method so that it returns the sum of all
 	//   of the integers in the array being passed in
 	public static int sumIntArray(int[] values) {
-
-		return 0;
+		int whatToReturn = 0;
+		for(int i = 0; i<values.length;i++) {
+			whatToReturn+=values[i];
+		}
+		return whatToReturn;
 	}
 	
 	//2. Complete the method so that it returns the average of all
 	//   of the integers in the array being passed in
 	public static double averageIntArray(int[] values) {
-				
-		return 0;
+		double whatToReturn = 0;
+		for(int i = 0; i<values.length;i++) {
+			whatToReturn+=values[i];
+		}
+		whatToReturn = whatToReturn/values.length;
+		return whatToReturn;
 	}
 	
 	
@@ -20,15 +27,26 @@ public class _00_1D_Array_Methods {
 	//   array contains the value specified by the second parameter.
 	//   It should otherwise return false.
 	public static boolean containsIntValue(int[] array, int value) {
-	
-		return false;
+		boolean whatToReturn = false;
+		for(int i = 0; i<array.length; i++) {
+			if(array[i] == value) {
+				whatToReturn = true;
+			}
+		}
+		return whatToReturn;
 	}
 	
 	//4. Complete the method so that it returns the index of the,
 	//   first instance that the specified value occurs in the array.
 	//   If the array does not contain the specified value, it should return -1.
 	public static int getIndex(int[] arr, int value) {
-		
-		return 0;
+		int whatToReturn = -1;
+		for(int i = 0; i<arr.length; i++) {
+			if(arr[i] == value) {
+				whatToReturn = i;
+				break;
+			}
+		}
+		return whatToReturn;
 	}
 }
--- src/_03_More_Array_Fun/MoreArrayFun.java
+++ src/_03_More_Array_Fun/MoreArrayFun.java
@@ -4,17 +4,29 @@ import java.util.Random;
 
 public class MoreArrayFun {
 	//1. Create a main method to test the other methods you write.
-	
+	public static void main(String[] args) {
+		String[] strings = {"hello", "my", "name", "is", "Lucas"};
+		new MoreArrayFun().printStrings(strings);
+		new MoreArrayFun().printBackwardsStrings(strings);
+	}
 	
 	
 	//2. Write a method that takes an array of Strings and prints all the Strings in the array.
 	
-	
+	void printStrings(String[] strings) {
+		for(int i = 0; i<strings.length; i++) {
+			System.out.println();	
+		}
+	}
 	
 	//3. Write a method that takes an array of Strings and prints all the Strings in the array
 	//   in reverse order.
 	
-	
+	void printBackwardsStrings(String[] strings) {
+		for(int i = strings.length-1; i>0; i--) {
+			System.out.println();	
+		}
+	}
 	
 	//4. Write a method that takes an array of Strings and prints every other String in the array.
 	
